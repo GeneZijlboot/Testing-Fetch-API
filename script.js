d41b4805c3d3f4519f0d3ae14efe743dd04c86ba
GeneZijlboot <gene.cyberzijlmans@gmail.com>
2023-07-22 18:33:25 +0200

    Done

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 let img = document.querySelector('img');
+const Container = document.querySelector('#Container');
 const Refreshbtn = document.querySelector('#RefreshImg');
 const NewGiphy = document.querySelector('#NewGiphy');
 let UserInput = document.querySelector('#UserInput');
@@ -6,23 +7,29 @@ let UserInput = document.querySelector('#UserInput');
 Refreshbtn.addEventListener('click', RefreshGiphy);
 NewGiphy.addEventListener('click', DifferentGiphy);
 
-function RefreshGiphy(){
-    fetch('https://api.giphy.com/v1/gifs/translate?api_key=4Cdrh3upRQiptnX30px7gGTsqEa0Hcoh&s=cat', {mode: 'cors'})
+function RefreshGiphy(UserInput){
+    fetch('https://api.giphy.com/v1/gifs/translate?api_key=4Cdrh3upRQiptnX30px7gGTsqEa0Hcoh&s=' + UserInput, {mode: 'cors'})
     .then(function(response) {
         return response.json()
     })
     .then(function(response){
         img.src = response.data.images.original.url;
     })
-    .catch(function(e) {
-        console.log(e);
-    })
+    .catch(ErrorMsg);
 }
 
 function DifferentGiphy(e){
     e.preventDefault();
-    console.log(UserInput.value);
+    RefreshGiphy(UserInput.value);
     UserInput.value = '';
+}
+
+function ErrorMsg(){
+    Container.removeChild(img);
+    const Text = document.createElement('p');
+    Text.textContent = 'sorry, couldnt find any Giphys';
+    Text.id = 'TextStyle';
+    Container.appendChild(Text);
 }
 
 RefreshGiphy();
